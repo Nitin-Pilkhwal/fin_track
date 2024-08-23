ed8f43cdf90ab2e1932c92b5953cb557f4520f48
Nitin Pilkhwal <1nasty.np@gmail.com>
2024-08-24 00:55:42 +0530

    updating setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ pip install setuptools
 pip install -r requirements.txt
 
 #Run django commands
-python manage.py generate_transactions
-python manage.py makemigrations
-python manage.py migrate
 python manage.py collectstatic --no-input
+python manage.py migrate
 python manage.py runserver
