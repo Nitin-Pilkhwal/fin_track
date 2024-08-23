789e0dc4f6db9172d6753bd717c4b3c9a44d6a73
Nitin Pilkhwal <1nasty.np@gmail.com>
2024-08-23 23:12:32 +0530

    no input

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 python manage.py generate_transactions
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 python manage.py runserver
