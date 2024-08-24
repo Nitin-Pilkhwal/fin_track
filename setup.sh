a16bd8c758e8a4c0385d29bc98dafcb106e4abff
Nitin Pilkhwal <1nasty.np@gmail.com>
2024-08-24 18:30:49 +0530

    render deploy changes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,31 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install setuptools
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Run Django management commands
-python manage.py collectstatic --noinput
-python manage.py migrate
+# Convert static asset files
+python manage.py collectstatic --no-input
 
-# Start Gunicorn server
-gunicorn finance_project.wsgi:application --bind 0.0.0.0:$PORT
+# Apply any outstanding database migrations
+python manage.py migrate
+#!/bin/bash
+
+
+
+
+# Exit on error
+# set -o errexit
+
+# # Upgrade pip and install dependencies
+# pip install --upgrade pip
+# pip install setuptools
+# pip install -r requirements.txt
+
+# # Run Django management commands
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+
+# # Start Gunicorn server
+# gunicorn finance_project.wsgi:application --bind 0.0.0.0:$PORT
