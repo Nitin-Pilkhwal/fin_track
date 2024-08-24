b7eb10b635da6883031dde0c98e0ff90a4e5a72c
Nitin Pilkhwal <1nasty.np@gmail.com>
2024-08-24 13:09:35 +0530

    test2

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Exit on error
-set -o errexit
-
 # Install dependencies
-pip install --upgrade pip
 pip install setuptools
 pip install -r requirements.txt
 
@@ -13,8 +9,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Start the server
-# Typically, for production, you use a WSGI server like Gunicorn, not Django's runserver
-# This command is generally used for development purposes
-# Replace the line below with your production WSGI server command
-pip install gunicorn
-gunicorn finance_project.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver
